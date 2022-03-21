28b14a806bd6ffca001dd14528e3a857e42792c3
toningabriel <ritadebrito.capanema@gmail.com>
2022-03-21 10:23:08 -0300

    Initial commit

---
+++ aula1.py
@@ -0,0 +1,19 @@
+#funcao de imprimir na tela (usando aspas duplas e simples)
+print("alo mundo")
+print('oi ja programo')
+#operadores logicos
+print(2+2)
+print(2*2)
+print(5-2)
+print(10/2)
+print(2**2)
+print(10%3)
+#sinais de comparacao
+print(4==4)
+print(4==5)
+print(5!=5)
+print(5!=9)
+print(10>=10)
+print(10>=9)
+print(8<9)
+"""estou na aula de logica e programacao eba"""
